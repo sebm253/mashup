5e118fc0db279ed499954e1190271d904c51eee3
mlnrDev <git@mlnr.dev>
2024-02-17 13:39:09 +0100

    feat(utils): use pointers for structs

--- utils.go
+++ utils.go
@@ -5,16 +5,16 @@ import (
 	"sort"
 )
 
-func getProminentImageColors(image image.Image) (pixelMap map[pixelData][]coordsData, keys []pixelData) { // map of RGBA:slice of all cords
+func getProminentImageColors(image image.Image) (pixelMap map[*pixelData][]*coordsData, keys []*pixelData) { // map of RGBA:slice of all cords
 	bounds := image.Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
 
-	pixelMap = make(map[pixelData][]coordsData)
+	pixelMap = make(map[*pixelData][]*coordsData)
 	for y := 0; y < height; y++ { // loop through all pixels
 		for x := 0; x < width; x++ {
 			rgbaPixel := rgbaToPixel(image.At(x, y).RGBA())
 			coords := pixelMap[rgbaPixel]
-			pixelMap[rgbaPixel] = append(coords, coordsData{X: x, Y: y})
+			pixelMap[rgbaPixel] = append(coords, &coordsData{X: x, Y: y})
 		}
 	}
 	keys = mapKeys(pixelMap)
@@ -24,12 +24,12 @@ func getProminentImageColors(image image.Image) (pixelMap map[pixelData][]coords
 	return
 }
 
-func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) pixelData {
-	return pixelData{uint8(r / 257), uint8(g / 257), uint8(b / 257), uint8(a / 257)}
+func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) *pixelData {
+	return &pixelData{uint8(r / 257), uint8(g / 257), uint8(b / 257), uint8(a / 257)}
 }
 
-func mapKeys(m map[pixelData][]coordsData) []pixelData {
-	s := make([]pixelData, 0, len(m))
+func mapKeys(m map[*pixelData][]*coordsData) []*pixelData {
+	s := make([]*pixelData, 0, len(m))
 	for k := range m {
 		s = append(s, k)
 	}
